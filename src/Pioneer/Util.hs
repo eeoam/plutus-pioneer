1cc2729e05cee466d52c350562895c8a94f80215
Eric <eric@whetztone.net>
2023-11-22 12:44:09 +0000

    Added FortyTwoTyped.

--- src/Pioneer/Util.hs
+++ src/Pioneer/Util.hs
@@ -19,6 +19,15 @@ import Cardano.Api.Shelley
     ( PlutusScript (..)
     )
 
+import PlutusTx
+    ( BuiltinData
+    , UnsafeFromData (..)
+    )
+
+import PlutusTx.Prelude
+    ( traceError
+    )
+
 import Plutus.V2.Ledger.Api qualified as PlutusV2
 
 writeValidatorToFile :: FilePath -> PlutusV2.Validator -> IO ()
@@ -37,3 +46,17 @@ validatorToScript = serializableToScript
 serializableToScript :: Serialise a => a -> PlutusScript PlutusScriptV2
 serializableToScript = PlutusScriptSerialised . BSS.toShort . BSL.toStrict . serialise
 
+-- | A more efficient implementation of the `mkUntypedMintingPolicy` method of the `IsScriptContext` typeclass.
+-- Copyright (c) 2023 Ian Burzynski
+{-# INLINABLE mkUntypedScript #-}
+mkUntypedScript :: ( UnsafeFromData a, UnsafeFromData b)
+                   => (a -> b -> PlutusV2.ScriptContext -> Bool)
+                   -> (BuiltinData -> BuiltinData -> BuiltinData -> ())
+mkUntypedScript f a b ctx =
+    check $
+        f
+            (unsafeFromBuiltinData a)
+            (unsafeFromBuiltinData b)
+            (unsafeFromBuiltinData ctx)
+
+check b = if b then () else traceError "script validation failed"
---
+++ src/Pioneer/Week02/FortyTwoTyped.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Pioneer.Week02.FortyTwoTyped where
+
+import PlutusTx 
+    ( BuiltinData
+    , compile
+    )
+
+import PlutusTx.Prelude
+    ( traceError, traceIfFalse
+    , otherwise
+    , (==)
+    , Bool
+    , Integer
+    , ($)
+    )
+
+import PlutusTx.Builtins
+    ( mkI
+    )
+
+import Plutus.V2.Ledger.Api
+    ( Validator
+    , mkValidatorScript
+    , ScriptContext(..)
+    )
+
+import Pioneer.Util
+
+import Prelude (IO)
+
+-- | This validator succeeds only if the redeemer is 42.
+{-# INLINABLE script #-}
+script :: () -> Integer -> ScriptContext -> Bool
+script _ r _ = traceIfFalse "expected 42" $ r == 42
+
+{-# INLINABLE script_untyped #-}
+script_untyped :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+script_untyped = mkUntypedScript script
+
+validator :: Validator
+validator = mkValidatorScript $$(compile [||script_untyped||])
+
+save :: IO ()
+save = writeValidatorToFile "./assets/fortytwotyped.plutus" validator
+
